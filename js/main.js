6ad961bda35792c2359d17079a6c96b7d6b091d1
T. Lanette <TLPollard4516@gmail.com>
2020-06-27 19:31:59 -0400

    Update style for theme switch
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 //Toggle Switch
+
+const checkbox = document.querySelector(".checkbox");
+console.log(checkbox);
+
+checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        transition();
+        document.documentElement.setAttribute("data-theme", "light");
+    } else {
+        transition();
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+});
+
+const transition = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+        document.documentElement.classList.remove("transition");
+    }, 1000);
+};
