ccefee64adf64973cd7faea81795be703da85057
T. Lanette <TLPollard4516@gmail.com>
2020-06-27 22:03:43 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 //Toggle Switch
+let checkbox = document.getElementById('dark-theme-toggle');
+let body = document.querySelector('body');
+
+checkbox.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+    }
+});
