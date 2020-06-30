64c97662f3f914ba629635cf00991e59be87b0e1
T. Lanette <TLPollard4516@gmail.com>
2020-06-29 21:42:23 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 //Toggle Switch
+const switchToggle = document.querySelector(".switch-wrapper");
+const body = document.querySelector("body");
+
+switchToggle.addEventListener("click", () => {
+    // Add dark theme
+    body.classList.toggle("dark");
+})
